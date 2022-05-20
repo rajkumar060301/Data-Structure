45e0fdbfe2ecfb2a863ff25a799fb98d64dd85ef
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-05-21 01:22:39 +0530

    create length method

--- src/singlyLinkedList/LinkedList.java
+++ src/singlyLinkedList/LinkedList.java
@@ -4,30 +4,11 @@ package singlyLinkedList;/*
  * JDK: 18 version
  * Date: 20-May-22
  */
+
 // Make a linked list class
 public class LinkedList {
-    private  Node  head;
+    private Node head;
 
-    private static class Node{
-        private int data;
-        private Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-
-    }
-    // Print the element of linked list
-    public void display(){
-        Node current = head;
-        while (current!=null){
-            System.out.print(current.data + "->");
-            current = current.next;
-        }
-        System.out.println("null");
-
-    }
     public static void main(String[] args) {
         LinkedList sll = new LinkedList();
         // Insert the Element in linked list
@@ -39,6 +20,42 @@ public class LinkedList {
         secondNode.next = thirdNode;
         thirdNode.next = forthNode;
         sll.display();
+        System.out.println("Length: " + sll.length());
+
+    }
+
+    // Print the element of linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+
+    }
+
+    public int length() {
+        if (head == null) {
+            return 0;
+        }
+        Node current = head;
+        int count = 0;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
 
     }
 
