4ee287c53e8b51a13fd7039d4ab1752762eb8f49
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-06 19:56:52 +0530

    selectionsort completed in java

---
+++ src/sorting/Selection/SelectionSort.java
@@ -0,0 +1,30 @@
+package sorting.Selection;/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 18 version
+ * Date: 20-Jun-22
+ */
+
+
+public class SelectionSort extends Main {
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+    }
+
+    static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+
+        }
+
+    }
+}
