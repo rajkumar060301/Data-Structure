6cd1e230697f8261757fc1508025bb4bbbc1d127
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-06 19:54:30 +0530

    ready for linklist

--- src/singlyLinkedList/LinkedList.java
+++ src/singlyLinkedList/LinkedList.java
@@ -9,6 +9,7 @@ package singlyLinkedList;/*
 public class LinkedList {
     private Node head;
 
+
     public static void main(String[] args) {
         LinkedList sll = new LinkedList();
         // Insert the Element in linked list
@@ -19,8 +20,136 @@ public class LinkedList {
         sll.head.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = forthNode;
+        System.out.println("Print LinkedList");
         sll.display();
         System.out.println("Length: " + sll.length());
+
+        // Add First node
+        sll.insertFirst(20);
+        sll.insertFirst(86);
+        sll.insertFirst(87);
+        sll.insertFirst(47);
+        sll.insertFirst(86);
+        System.out.println("Add first element");
+        sll.display();
+
+        // add last node
+        sll.addLast(25);
+        sll.addLast(10);
+        sll.addLast(85);
+        System.out.println("Add Last element");
+        sll.display();
+
+        //add given position
+        sll.addGivenPosition(2, 12);
+        System.out.println("Insert element at given position");
+        sll.display();
+
+        // Delete first element
+        sll.deleteFirst();
+        System.out.println("Delete first element");
+        sll.display();
+
+        // Delete Last node
+        sll.deleteLast();
+        System.out.println("Delete last node");
+        sll.display();
+
+        // delete at given position
+        sll.deleteGivenPosition(2);
+        System.out.println("Delete at given position");
+        sll.display();
+
+        // search element in linkedList
+//        System.out.println("Data found");
+        if (sll.searchElement(99)) {
+            System.out.println("Element is founded");
+        } else {
+            System.out.println("Element is not found");
+        }
+        //reverse the linkedList
+        System.out.println("Reverse the linkedList");
+        Node reverseNode = sll.reverse(sll.head);
+        sll.display();
+
+
+    }
+
+    public Node reverse(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Node current = head;
+        Node previous = null;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+
+    }
+
+    public boolean searchElement(int key) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void deleteGivenPosition(int pos) {
+        if (pos == 1) {
+            head = head.next;
+        } else {
+            Node previous = head;
+            int count = 1;
+            while (count < pos - 1) {
+                previous = previous.next;
+                count++;
+            }
+            Node current = previous.next;
+            previous.next = current.next;
+        }
+
+    }
+
+    public Node deleteLast() {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node current = head;
+        Node previous = null;
+        while (current.next != null) {
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        return current;
+
+    }
+
+    private void addGivenPosition(int i, int new_data) {
+        Node node = new Node(new_data);
+        if (head == null) {
+            node.next = head;
+            head = node;
+        } else {
+            Node previous = head;
+            int count = 1;
+            while (count < i - 1) {
+                previous = previous.next;
+                count++;
+            }
+            Node current = previous.next;
+            previous.next = node;
+            node.next = current;
+        }
 
     }
 
@@ -48,6 +177,50 @@ public class LinkedList {
         return count;
     }
 
+    public void insertFirst(int new_data) {
+        Node new_Node = new Node(new_data);
+        new_Node.next = head;
+        head = new_Node;
+    }
+
+    public Node deleteFirst() {
+//        Node newNode = new Node(element);
+        if (head == null) {
+            return null;
+        }
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        return temp;
+    }
+
+    public void addLast(int element) {
+        Node new_Node = new Node(element);
+        if (head == null) {
+            head = new Node(element);
+        } else {
+            new_Node.next = null;
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_Node;
+        }
+    }
+
+    public void addAfterGivenNode(Node givenNode, int new_data) {
+        Node new_node = new Node(new_data);
+        if (givenNode == null) {
+            System.out.println("null");
+            return;
+        } else {
+            new_node.next = givenNode.next;
+            givenNode.next = new_node;
+
+        }
+
+    }
+
     private static class Node {
         private int data;
         private Node next;
@@ -58,5 +231,4 @@ public class LinkedList {
         }
 
     }
-
 }
