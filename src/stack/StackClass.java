1aa50f0434d038a9d7943ad8a5d23b6d3f37dba2
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-08 23:37:32 +0530

    stack class

---
+++ src/stack/StackClass.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 08-Jan-23
+ */public class StackClass {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(48);
+        stack.push(98);
+        stack.push(35);
+        stack.push(29);
+        stack.push(78);
+        stack.push(76);
+        System.out.println(stack);
+    }
+}
