08030a003fb67a9442a6275753994e9d1e51625e
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-08 23:31:25 +0530

    hello rajkumar

---
+++ src/Faltu.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 08-Jan-23
+ */public class Faltu {
+    public static void main(String[] args) {
+        System.out.println("Hii Raj Kumar");
+    }
+}
