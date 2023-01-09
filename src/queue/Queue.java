9cb4b560b3a3d6337c96d36b095b0d6640daf78b
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-09 17:13:40 +0530

    queue commit

---
+++ src/queue/Queue.java
@@ -0,0 +1,14 @@
+package queue;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 08-Jan-23
+ */
+public class Queue {
+    public static void main(String[] args) {
+
+    }
+
+}
