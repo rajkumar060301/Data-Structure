c04e0d3883f3538af9d472da8fc88859f4471235
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-12 21:09:17 +0530

    print array odd arary

---
+++ src/array/ArrayEvenNumber.java
@@ -0,0 +1,12 @@
+package array;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 12-Jan-23
+ */public class ArrayEvenNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello Raj Kumar");
+    }
+}
