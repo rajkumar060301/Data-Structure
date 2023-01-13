bb632b5bcec439163b41ba89c151108ea8f06004
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-13 16:11:04 +0530

    hello commit

---
+++ src/array/Hello.java
@@ -0,0 +1,12 @@
+package array;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 13-Jan-23
+ */public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
