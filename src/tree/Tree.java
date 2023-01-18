ac4beaf6e325892354097e89042540c668876c7c
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-18 21:19:17 +0530

    tree data structure

---
+++ src/tree/Tree.java
@@ -0,0 +1,13 @@
+package tree;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 19 version
+ * Date: 18-Jan-23
+ */public class Tree {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        System.out.println("Hello Tree Data Structure");
+    }
+}
