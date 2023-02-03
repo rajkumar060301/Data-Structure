28af7a7581cfbeaafc1be1f92993d58dc1d1a319
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-02-03 11:18:55 +0530

    queue class and ListNode, created enqueue method

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,5 +1,4 @@
 package queue;
-
 /*
  * Author Name: Raj Kumar
  * IDE: IntelliJ IDEA Ultimate Edition
@@ -7,8 +6,69 @@ package queue;
  * Date: 08-Jan-23
  */
 public class Queue {
-    public static void main(String[] args) {
+    private ListNode front;
+    private ListNode rear;
+    private int length;
 
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(54);
+        queue.enqueue(79);
+        queue.enqueue(37);
+        queue.enqueue(46);
+        queue.printQueue();
+
+
+    }
+
+    public void enqueue(int data) {
+        ListNode temp = new ListNode(data);
+        if (isEmpty()) {
+            front = temp;
+        } else {
+            rear.next = temp;
+        }
+        rear = temp;
+        length++;
+    }
+
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+
+    public void printQueue() {
+        if (isEmpty()) {
+            return;
+        }
+        ListNode current = front;
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
     }
 
 }
